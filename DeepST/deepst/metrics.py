e0177d23edfde91d041094cdb7a61289de029a92
Gary Goh <garygsw@gmail.com>
2018-02-08 13:50:07 +0800

    revised metrics to support tensorflow

--- DeepST/deepst/metrics.py
+++ DeepST/deepst/metrics.py
@@ -23,16 +23,34 @@ mse = MSE = mean_squared_error
 
 
 def masked_rmse(mask):
-    def masked_rmse(y_true, y_pred):
-        idx = K.tile(mask, [y_true.shape[0], 1, 1, 1])
-        idx = idx.nonzero()  # to make it a tensor mask
-        return K.mean(K.square(y_pred[idx] - y_true[idx])) ** 0.5
+    if K.backend() == 'tensorflow':
+        def masked_rmse(y_true, y_pred):
+            idx = K.expand_dims(mask, axis=0)
+            idx = K.tile(idx, [K.shape(y_true)[0], 1, 1, 1])
+            idx = K.cast(idx, 'float32')
+            zero = K.equal(idx, K.constant(1, dtype='float32'))
+            zero = K.cast(zero, 'float32')
+            return (K.sum(K.square(y_pred - y_true) * zero) / K.sum(idx)) ** 0.5
+    elif K.backend() == 'theano':
+        def masked_rmse(y_true, y_pred):
+            idx = K.tile(mask, [y_true.shape[0], 1, 1, 1])
+            idx = idx.nonzero()  # to make it a tensor mask
+            return K.mean(K.square(y_pred[idx] - y_true[idx])) ** 0.5
     return masked_rmse
 
 
 def masked_mse(mask):
-    def masked_mse(y_true, y_pred):
-        idx = K.tile(mask, [y_true.shape[0], 1, 1, 1])
-        idx = idx.nonzero()  # to make it a tensor mask
-        return K.mean(K.square(y_pred[idx] - y_true[idx]))
+    if K.backend() == 'tensorflow':
+        def masked_mse(y_true, y_pred):
+            idx = K.expand_dims(mask, axis=0)
+            idx = K.tile(idx, [K.shape(y_true)[0], 1, 1, 1])
+            idx = K.cast(idx, 'float32')
+            zero = K.equal(idx, K.constant(1, dtype='float32'))
+            zero = K.cast(zero, 'float32')
+            return K.sum(K.square(y_pred - y_true) * zero) / K.sum(idx)
+    elif K.backend() == 'theano':
+        def masked_mse(y_true, y_pred):
+            idx = K.tile(mask, [y_true.shape[0], 1, 1, 1])
+            idx = idx.nonzero()  # to make it a tensor mask
+            return K.mean(K.square(y_pred[idx] - y_true[idx]))
     return masked_mse
